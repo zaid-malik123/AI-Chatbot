7d8db947a4a9db2eb9f7ce997f52afc66216a0a5
mohd zaid malik <mzaidmalik25@gmail.com>
2025-12-19 19:48:54 +0530

    feat(frontend): textarea UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import Sidebar from "./components/Sidebar";
-import { IoIosArrowDown } from "react-icons/io";
-import { RiUserAddLine } from "react-icons/ri";
-import { FiMessageCircle } from "react-icons/fi";
+import TextArea from "./components/TextArea";
+
 const App = () => {
 
   
@@ -9,19 +8,8 @@ const App = () => {
     <div className="w-screen h-screen bg-[#212121] text-text flex">
       
       <Sidebar/>
-
-      <div className="w-[80%] h-full ">
-         <header className="w-full flex items-center justify-between p-5">
-            <div className="flex text-white items-center gap-2">
-               <h2 className="text-xl font-[300]">Nova</h2>
-               <IoIosArrowDown size={20} />
-            </div>
-            <div className="flex items-center gap-4">
-              <RiUserAddLine size={20} color="white" />
-              <FiMessageCircle size={20} color="white" />
-            </div>
-         </header>
-      </div>
+      
+      <TextArea/>
     </div>
   );
 };
